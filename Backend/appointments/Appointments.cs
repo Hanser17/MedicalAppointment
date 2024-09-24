b11dace65a0d48a2695850dea375ad9a416110d9
Hanser17 <Hanser.pimentel@gmail.com>
2024-09-23 22:11:37 -0400

    creacion de clases

---
+++ Backend/appointments/Appointments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointment.Classes
+{
+    internal class Appointments
+    {
+        public int AppointmentID { get; set; }
+        public int PatientID { get; set; }
+        public int AppointmentDate { get; set; }
+        public int StatusID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+      
+    }
+}
---
+++ Backend/appointments/DoctorAvailability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAppointment.Classes
+{
+     class DoctorAvailability
+    {
+        public int AvailabilityID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AvailableDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
